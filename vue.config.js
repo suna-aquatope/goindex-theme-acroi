ce623b407eabc33f6823035475b82a6514c1268c
Kusue X Code <92562821+suna-aquatope@users.noreply.github.com>
2022-02-01 18:00:59 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -113,7 +113,7 @@ module.exports = {
     publicPath,
     proxy: {
       "/api": {
-        target: "https://ossdev.achirou.workers.dev/",
+        target: "https://idxpoi.aozorarmx.workers.dev/",
         ws: true,
         changeOrigin: true,
         pathRewrite: {
